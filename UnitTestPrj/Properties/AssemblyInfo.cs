df0c134a0869eb55fd6d81428b793dc0a3747443
gzh205 <1942592358@qq.com>
2019-10-06 15:47:29 +0800

    测试了设备模拟功能，并且添加了图片下载功能

---
+++ UnitTestPrj/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestPrj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestPrj")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f0539a7-ad86-4345-af22-cc92fe262dbd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestPrj/Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace UnitTestPrj
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void testUA()
+        {
+            Crawler.Documents doc = new Crawler.Documents("https://nbx2.kongzhong.com/r/jw/180319110923", Crawler.Documents.AndroidUA);//使用安卓手机访问百度移动网站
+            writeIntoFile("testua.html",doc.geHtmlDoc());
+        }
+        [TestMethod]
+        public void testDownloadImage()
+        {
+            Crawler.Documents.SaveImage("tmp.bmp",Crawler.Documents.GetImage("http://a.hiphotos.baidu.com/image/pic/item/838ba61ea8d3fd1fc9c7b6853a4e251f94ca5f46.jpg"));
+        }
+        /// <summary>
+        /// 将字符串写进文件中
+        /// </summary>
+        /// <param name="filename">文件名称</param>
+        /// <param name="text">待写入的文本</param>
+        public void writeIntoFile(string filename,string text)
+        {
+            FileStream fs = new FileStream(filename, FileMode.OpenOrCreate);
+            fs.Seek(0, SeekOrigin.Begin);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(text);
+            sw.Close();
+        }
+    }
+}
